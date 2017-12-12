1e30327930f18351543bea2a9f219baf0f438410
Aditya Dutt <duttaditya18@gmail.com>
2017-12-12 21:36:24 +0530

    Some flask tuff

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('layout.html')
+	return render_template('home.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)

dc62cd910b4164d201550ce97999f59c578da3eb
agilephoenix <agilephoenix815.dk@gmail.com>
2017-12-13 13:41:18 +0530

    added new routes

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-
+ 
 @app.route('/')
-def index():
+def index(): 
 	return render_template('home.html')
+
+@app.route('/user/<user>')
+def channel(user):
+	return render_template('user.html',data=data)
+
+@app.route('/<blogpost>')
+def view_post(blogpost):
+	return render_template('blogpost.html',data=blogpost)
+	
+
 
 if __name__ == '__main__':
 	app.run(debug=True)

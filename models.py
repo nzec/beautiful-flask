add1fa2eac5e0877e856975624ac25471e1c46d5
datboiee <ishaan56verma@gmail.com>
2017-12-23 09:24:43 +0530

    Models. blogpost working

--- models.py
+++ models.py
@@ -7,10 +7,11 @@ class Blogpost:
 		self.author = author
 		self.datetime = time.ctime()
 		self.timestamp = time.time()
-		#self.post_id = self.remove_spaces() + ".html"
+		self.post_directory = self.make_directory()
+		self.post_id = self.remove_spaces() + ".html"
 
 	def write_to_file(self,content):
-		self.post_directory = self.make_directory()
+		self.content = content		
 		self.file = open(os.path.join(self.post_directory,self.title),'w')
 		self.file.write("""
 			<title>{}</title>
@@ -22,13 +23,15 @@ class Blogpost:
 
 	def remove_spaces(self):
 		for char in range(len(self.title)):
+			print(char)
 			if self.title[char] == " ":
-				self.title[char] = "+"
+				self.title[char] = ""
+		return self.title
 	
 	def make_directory(self):
 		try:
 			os.mkdir(os.path.join(os.path.join('templates','blogposts'),self.author))
 		except:
 			pass
-		self.post_directory = os.path.join(os.path.join('templates','blogposts'),self.author)
+		return os.path.join(os.path.join('templates','blogposts'),self.author)
 
